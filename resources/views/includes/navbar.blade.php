e35d2759d92e771cb7332a9f4a47ff136aeef3a3
Omar Iriskic <noisewavehd@gmail.com>
2019-08-22 00:24:05 +0200

    HTML structure of Landing page

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,11 @@
+<header class="header">
+    <img src="img/logo.png" alt="Deezer logo" class="logo">
+    <nav class="navigation">
+        <ul class="navigation__list">
+            <li class="navigation__item"><a href="/download" class="navigation__link">Download</a></li>
+            <li class="navigation__item"><a href="/plans" class="navigation__link">Plans</a></li>
+            <li class="navigation__item"><a href="/login" class="navigation__button--secondary--blue">Log in</a></li> 
+            <li class="navigation__item"><a href="/register" class="navigation__button--primary-blue">Sign up</a></li> 
+        </ul>
+    </nav>
+</header>
